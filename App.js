ac43e8ae4df102bf164e2f97f2ddfd83f33a3314
Enthony <oliveiraenthony987@gmail.com>
2025-11-26 16:17:13 -0300

    primeiro projeto mobile

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.appbar}>
+        <Text style={styles.appbartext}>Eu Sou Programador</Text>
+      </View>
+
+      <View style={styles.content}>
+        <Image
+          source={require("./assets/image/react-avatar.png")}
+          style={styles.Image} />
+      </View>
+
     </View>
   );
 }
@@ -13,8 +21,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F2F2F2'
   },
+
+  appbar: {
+    height: 70,
+    backgroundColor: '#3B82F6',
+    justifyContent: 'center',
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    elevation: 4
+  },
+
+  appbartext: {
+    color: '#fff',
+    textAlign: 'center',
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  Image: {
+    width: 300,
+    height: 300
+  }
 });
